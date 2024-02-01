6b4ea3f12297e3b4ad909240ba507f57bf44ed0e
gleb 33 <65718268+glebanya13@users.noreply.github.com>
2024-02-01 13:07:51 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,28 @@
-const Pool = require('pg').Pool
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
-module.exports = pool
+const { Sequelize } = require('sequelize');
+
+if (!process.env.POSTGRES_URL) {
+  console.error('POSTGRES_URL is not defined in the environment variables.');
+  process.exit(1);
+}
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL + "?sslmode=require", {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection to the database has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+    process.exit(1);
+  });
+
+module.exports = sequelize;
